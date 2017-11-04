b768cdace6497ac5cf0b7dd002a11f6432129252
emir8gallo <b8emir@gmail.com>
2017-11-04 23:41:53 +0100

    Kreiran projekat sa pocetnom i login formom, dodana dokumentacija, kreiran dump baze korisnika sa par korisnika i uloga

---
+++ DocumentManagementSystem/src/main/java/com/etf/dms/DocumentManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.etf.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.etf.dms", "controller", "config"})
+public class DocumentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagementSystemApplication.class, args);
+	}
+}
---
+++ DocumentManagementSystem/src/main/java/com/etf/dms/controller/WebController.java
@@ -0,0 +1,19 @@
+package com.etf.dms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+	
+	@RequestMapping(value={"/","home"})
+    public String home(){
+        return "home";
+    }
+   
+    @RequestMapping(value={"/login"})
+    public String login(){
+        return "login";
+    }
+
+}
