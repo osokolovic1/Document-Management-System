23ecf3a18a0336c65c16e4cadf245dd8963b2fa8
Omar Sokolovic <osokolovic1@etf.unsa.ba>
2017-11-07 16:01:33 +0100

    popravljen problem sa css-om

--- DocumentManagementSystem/src/main/java/com/etf/dms/DocumentManagementSystemApplication.java
+++ DocumentManagementSystem/src/main/java/com/etf/dms/DocumentManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.etf.dms", "controller", "config"})
+//@ComponentScan({"com.etf.dms", "controller", "config"})
 public class DocumentManagementSystemApplication {
 
 	public static void main(String[] args) {
