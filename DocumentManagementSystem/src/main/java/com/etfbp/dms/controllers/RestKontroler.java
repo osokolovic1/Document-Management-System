dd6f257cd3f85d6d115f4b84f9095ed47db4c6e8
ebeus <ebeus1@etf.unsa.ba>
2018-01-08 15:16:21 +0100

    REST Backend
    Dodati provjeru sesije

--- DocumentManagementSystem/src/main/java/com/etfbp/dms/controllers/RestKontroler.java
+++ DocumentManagementSystem/src/main/java/com/etfbp/dms/controllers/RestKontroler.java
@@ -9,27 +9,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.etfbp.dms.models.Document;
+import com.etfbp.dms.models.DocumentInfo;
 import com.etfbp.dms.models.FileBucket;
+import com.etfbp.dms.models.Grupa;
 import com.etfbp.dms.models.User;
+import com.etfbp.dms.models.UserInfo;
 import com.etfbp.dms.services.DocumentService;
 import com.etfbp.dms.services.GroupService;
 import com.etfbp.dms.services.UserService;
+import com.fasterxml.jackson.annotation.JsonRootName;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -110,6 +119,18 @@ public class RestKontroler {
     	return user;
     }
     
+    @RequestMapping(value = "/user/info/all", method = RequestMethod.GET) 
+    public List<UserInfo> userInfo() {
+    	List<UserInfo> usersInfo = new ArrayList<>();
+    	Set<User> users = userService.findAll();
+    	Iterator<User> iterator = users.iterator();
+    	while(iterator.hasNext()) {
+    		User element = iterator.next();
+    		usersInfo.add(new UserInfo(element.getID(), element.getName(), element.getLastName(), element.getEmail()));
+    	}
+    	return usersInfo;
+    }
+    
     @RequestMapping(value= "/documents/view/{docId}", method=RequestMethod.GET)
     public String viewDocument (@PathVariable String docId, HttpServletResponse response, HttpSession session) throws IOException{
         if(session.getAttribute("userid") == null)
@@ -118,6 +139,7 @@ public class RestKontroler {
     	Document document = documentService.findById(Integer.parseInt(docId));
     	if(document == null)
     		return "Not found";
+    	
     	
     	Boolean imaPravoPristupa = false;
         Iterator<User> iterator = document.getUsers().iterator();
@@ -146,20 +168,106 @@ public class RestKontroler {
         return null;
     }
     
+    //users documents
+    @RequestMapping(value="/documents/my-documents", method = RequestMethod.GET)
+    public List<DocumentInfo> getMyDocuments(HttpSession session) {   	
+    	int userId;// = (int)session.getAttribute("userid");
+    	userId = 2;
+    	
+    	
+    	List<DocumentInfo> documentInfo = new ArrayList<>();
+    	List<Document> docs = documentService.findAllByUserPermission(userId);
+    	for(Iterator<Document> it = docs.iterator(); it.hasNext(); ) {  
+    		Document d = it.next();
+    		documentInfo.add(new DocumentInfo(d.getId(), d.getName(), d.getDescription(), d.getType()));
+    	}
+    	
+    	return documentInfo;
+    	
+    }
+    //documents shared with user
     @RequestMapping(value = { "/documents/shared" }, method = RequestMethod.GET)
-    public Map<Integer, String> getSharedDocuments(HttpServletRequest request, HttpServletResponse response) {
+    public List<DocumentInfo> getSharedDocuments(HttpServletRequest request, HttpServletResponse response) {
 
     	HttpSession session = request.getSession(true);
     	
-    	int userId = (int)session.getAttribute("userid");
-    	 
- 
-        Map<Integer, String> listaDokumenata = new HashMap<>();
-        List<Document> docs = documentService.findAllByUserPermissionNotOwning(userId);
+    	int userId;// = (int)session.getAttribute("userid");
+    	userId = 2;
+    	List<DocumentInfo> documentInfo = new ArrayList<>();
+    	
+        List<Document> docs = documentService.findAllByUserId(userId);
         for(Iterator<Document> it = docs.iterator(); it.hasNext();) {
         	Document d = it.next();
-        	listaDokumenata.put(d.getId(), d.getName());
+        	documentInfo.add(new DocumentInfo(d.getId(), d.getName(), d.getDescription(), d.getType()));
         }
-        return listaDokumenata;
+        return documentInfo;
+    }
+    
+    private void saveDocument(FileBucket fileBucket, User user, String fileDescription, Set<User> sharedWithUsers,
+    		Set<Grupa> sharedWithGroups) throws IOException{    
+        MultipartFile multipartFile = fileBucket.getFile();
+        Document document = new Document(user.getID(),
+        							multipartFile.getOriginalFilename(),
+        							fileDescription,
+        							multipartFile.getContentType(),
+        							multipartFile.getBytes(), 
+        							sharedWithUsers, 
+        							sharedWithGroups);
+        documentService.saveDocument(document);
+        System.out.println(document.getId());
+    }
+    
+    @RequestMapping(value = { "/documents/add" }, method = RequestMethod.POST)
+    public String addDocuments(@Valid FileBucket fileBucket, BindingResult result, ModelMap model, @RequestParam MultipartFile file, 
+    		@RequestParam(value="fileDescription") String description, HttpSession session, HttpServletRequest request) throws IOException{
+    	
+    	
+    	int userId;// = (int)session.getAttribute("userid");
+    	userId = 2;
+    	User user = userService.findById(userId);
+    	model.addAttribute(user);
+    	
+        if (result.hasErrors()) {
+            System.out.println("validation errors");
+ 
+           // List<Document> documents = documentService.findAll();
+            Set<Document> docs = userService.findAllUserDocumentsById(user.getID().intValue());
+            model.addAttribute("documents", docs);
+             
+            return "managedocuments";
+        } else {
+             
+            System.out.println("Fetching file");
+            
+            model.addAttribute("user", user);
+            
+            //otherUsers
+            //sharedGroups
+            String[] users = request.getParameterValues("otherUsers");
+            String[] groups = request.getParameterValues("sharedGroups");
+
+            Set<User> sharedWithUsers = new HashSet<User>();
+            Set<Grupa> sharedWithGroups = new HashSet<Grupa>();
+            /*Ovo ce trebati optimizirati*/
+            
+            if(users != null)
+            	for (String u : users) 
+            		sharedWithUsers.add(userService.findByUserName(u));
+            else
+            	sharedWithUsers = null;
+            
+            if(groups != null)
+            	for (String g : groups)
+            		sharedWithGroups.add(groupService.findByGroupName(g));
+            else
+            	sharedWithGroups = null;
+           
+            model.addAttribute("user", user);
+            fileBucket.setFile(file);
+
+            saveDocument(fileBucket, user, description, sharedWithUsers, sharedWithGroups);
+ 
+            return "OK";
+        }
     }
 }
---
+++ DocumentManagementSystem/src/main/java/com/etfbp/dms/models/DocumentInfo.java
@@ -0,0 +1,43 @@
+package com.etfbp.dms.models;
+
+public class DocumentInfo {
+	private Integer id;
+	private String name;
+	private String description;
+	private String type;
+
+	public DocumentInfo(Integer id, String name, String description, String type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.type = type;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
